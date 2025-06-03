3f44ebd3c92c7e2fbcc6fb3565938058724bbf3e
Vadim Bukstein <vadimbu@rafael.co.il>
2025-06-03 10:13:53 +0300

    Added configuration for load balancer

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,11 +12,14 @@ module "vpc" {
 
   enable_nat_gateway = true
   single_nat_gateway = true
+  enable_dns_hostnames = true
 
   public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/elb" = "1"
   }
   private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb" = "1"
   }
 }
