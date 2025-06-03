1b9fdea8cf62fe1b76b0558c23c79026c61e5221
Vadim Bukstein <vadimbu@rafael.co.il>
2025-06-03 15:43:11 +0300

    Added bump docekr version step

---
+++ app/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-infra",
+    version="0.0.1",
+    description="Wheather App",
+    url="https://github.com/vbukstein/weather-app.git",)
