b634a23ab156dc5899724dc1247799646ae1d1fa
Vadim Bukstein <vadimbu@rafael.co.il>
2025-06-05 13:39:20 +0300

    Add ci/cd for PR + bump major version

--- app/setup.py
+++ app/setup.py
@@ -5,6 +5,6 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-infra",
-    version="0.0.15",
+    version="1.0.0",
     description="Wheather App",
     url="https://github.com/vbukstein/weather-app.git",)
